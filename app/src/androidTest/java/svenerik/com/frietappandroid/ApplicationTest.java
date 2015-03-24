4edd1f1b78277e766d43120116da9664f6a37979
Erik Brandsma <erik.brandsma@outlook.com>
2015-03-24 17:05:16 +0100

    Start
    hooray :confetti_ball:

---
+++ app/src/androidTest/java/svenerik/com/frietappandroid/ApplicationTest.java
@@ -0,0 +1,13 @@
+package svenerik.com.frietappandroid;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/svenerik/com/frietappandroid/LoginActivity.java
@@ -0,0 +1,52 @@
+package svenerik.com.frietappandroid;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onLogin(View button){
+        EditText usernameField = (EditText) findViewById(R.id.usernameField);
+        EditText passwordField = (EditText) findViewById(R.id.passwordField);
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
+        Log.i(TAG, ("Logging in " + username + " : " + password));
+    }
+}
